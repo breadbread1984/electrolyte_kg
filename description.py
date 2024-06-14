c96a85837fed5dbe1e61ba3c97e285b38be542d5
breadbread1984 <breadbread1984@163.com>
2024-06-14 15:22:38 +0800

    update

--- description.py
+++ description.py
@@ -51,16 +51,16 @@ class GloveBoxOperation(Description):
   def to_string(self,):
     params = json.loads(self.node['params'])
     if self.node['type'] == 'moving':
-      s = f"在{params['atmosphere']}环境的Glove Box中将容器{params['source']}中的物质转移{params['amount']}{params['unit']}到容器{params['target']}中。"
+      s = f"在{params['atmosphere']}环境的Glove Box中将容器{self.node['source']}中的物质转移{params['amount']}{params['unit']}到容器{self.node['target']}中。"
     elif self.node['type'] == 'sealing':
-      s = f"在{params['atmosphere']}环境的Glove Box中对容器{params['target']}采用{params['method']}采用封闭。"
+      s = f"在{params['atmosphere']}环境的Glove Box中对容器{self.node['target']}采用{params['method']}采用封闭。"
     elif self.node['type'] == 'heating':
-      s = f"在{params['atmosphere']}环境的Glove Box中对容器{params['target']}进行加热，从室温到目标温度为{params['temperature']}{params['unit']}，"
+      s = f"在{params['atmosphere']}环境的Glove Box中对容器{self.node['target']}进行加热，从室温到目标温度为{params['temperature']}{params['unit']}，"
       if params['warmup seconds'] != 'none' and params['warmup rate'] != 'none':
         s += f"加热需要在{params['warmup seconds']}秒时间以{params['warmup rate']}速率加热到目标温度，"
       s += f"在目标温度需要维持{params['duration seconds']}秒。"
     elif self.node['type'] == 'cooling':
-      s = f"在{params['atmosphere']}环境的Glove Box中对容器{params['target']}进行冷却，到室温。"
+      s = f"在{params['atmosphere']}环境的Glove Box中对容器{self.node['target']}进行冷却，到室温。"
     else:
       raise Exception('unknown Glove Box operation!')
     return s
