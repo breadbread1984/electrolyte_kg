83f9d32876bbc6ae4ceee9625dd168906d508c18
breadbread1984 <breadbread1984@163.com>
2024-06-19 09:17:44 +0800

    update

--- description.py
+++ description.py
@@ -121,6 +121,8 @@ class Purify(Description):
       s = f"对容器{self.node['target']}进行煅烧，煅烧在温度{params['temperature']}{params['unit']}持续{params['seconds']}秒。"
       if 'warmup rate' in params:
         s += "升温速度为{params['warmup rate']}。"
+      if 'cooldown rate' in params:
+        s += "冷却速度为{params['cooldown rate']}。"
     else:
       raise Exception('unknown purify method!')
     return s
