a4a3b486f4182729b1742f1b52a39a9b02ecb2bc
breadbread1984 <breadbread1984@163.com>
2024-06-25 16:11:20 +0800

    update

--- restore.sh
+++ restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-HOST="neo4j://localhost:7687"
+HOST="neo4j+s://ce5c573a.databases.neo4j.io:7687"
 USERNAME="neo4j"
-PASSWORD="neo4j"
+PASSWORD="RdKzW-YnyyRd5h1Q6OoRQUDza5edKXrmqDzSTN7QkZg"
 DATABASE="neo4j"
 
 cypher-shell -a "$HOST" -u "$USERNAME" -p "$PASSWORD" -d "$DATABASE" "match (a)-[r]-(b) delete a,r,b"
