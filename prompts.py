a5d8ce528de1a993edb1128a0b2117d2f02452e9
breadbread1984 <breadbread1984@163.com>
2024-07-02 10:59:41 +0800

    update

--- prompts.py
+++ prompts.py
@@ -37,23 +37,6 @@ def exp_instruction_prompt(tokenizer):
       ]
     },
     {
-      "precursors": ["Li2S", "LiCl", "P2S5"],
-      "target": "Li6PS5Cl",
-      "steps": [
-        "准备zirconia grinding jar类型100ml容器c1。",
-        "在vacuum环境的Glove Box中对容器c1添加3.81g的Li2S。",
-        "在vaccuum环境的Glove Box中对容器c1添加4.68g的LiCl。",
-        "在vaccuum环境的Glove Box中对容器c1添加1.32g的P2S5。",
-        "在vaccuum环境的Glove Box中对容器c1采用rubber sealing gasket封闭。",
-        "采用planetary ball mill对容器c1内的样本进行研磨，研磨转速为550rpm，研磨时间为36000秒。",
-        "准备quartz tube类型600ml容器c2。",
-        "在vaccuum环境的Glove Box中从容器c1中提取10份，每份重量为2g的样本，每份压成薄片并转移到容器c2。",
-        "在vaccuum环境的Glove Box中对容器c2采用tube sealing machine封闭。",
-        "对容器c2进行煅烧，煅烧在温度260C持续36000秒。升温速度为2/min。冷却速度为2/min。",
-        "采用inductively coupled plasma spectrometer(ICP)设备对容器c2内的样本进行检测，结果需要显示Li:P:S:Cl元素按照单位mol计算的比例应该为6.0:1.0:5.0:1.0。"
-      ]
-    },
-    {
       "precursors": ['Li3N', 'LiCl'],
       "target": 'Li6N0.3Cl0.7',
       "steps": [
@@ -80,6 +63,27 @@ def exp_instruction_prompt(tokenizer):
         "在vaccuum环境的Glove Box中从容器c1中提取10份，每份重量为2g的样本，每份压成薄片并转移到容器c2。",
         "在vaccuum环境的Glove Box中对容器c2采用tube sealing machine封闭。",
         "对容器c2进行煅烧，煅烧在温度260C持续36000秒。升温速度为2/min。冷却速度为2/min。",
+        "采用inductively coupled plasma spectrometer(ICP)设备对容器c2内的样本进行检测，结果需要显示Li:N:Cl元素按照单位mol计算的比例应该为6.0:0.3:0.7。"
+      ]
+    },
+    {
+      "precursors": ["Li2S", "P2S5", "LiBr", "LiI", "MnS"],
+      "target": "Li6N0.3Cl0.7",
+      "steps": [
+        "准备zirconia grinding jar类型45ml容器c1。",
+        "向容器c1添加0.5450g前体物质Li2S。",
+        "向容器c1添加0.8857g前体物质P2S5。",
+        "向容器c1添加0.2768g前体物质LiBr。",
+        "向容器c1添加0.2844g前体物质LiI。",
+        "向容器c1添加0.0081g前体物质MnS。",
+        "向容器c1添加4.0g前体物质toluene。",
+        "采用planetary ball mill对容器c1内的样本进行研磨，研磨转速为500rpm，研磨时间为144000秒。",
+        "在argon环境的Glove Box中对容器c1添加8.6g的toluene。",
+        "准备bucket for plate heater类型45ml容器c2。",
+        "在argon环境的Glove Box中从容器c1转移样本到容器c2。",
+        "在argon环境的Glove Box中对容器c2进行沉淀，然后去除supernatant solvent。",
+        "在argon环境的Glove Box中对容器c2进行加热，从室温到目标温度为80C，在目标温度需要维持10800秒。",
+        "采用X-ray diffraction(XRD)设备对容器c2内的样品进行检测，结果应该在Li2S,P2S5,LiBr,LiI物质对应位置产生峰值。",
         "采用inductively coupled plasma spectrometer(ICP)设备对容器c2内的样本进行检测，结果需要显示Li:N:Cl元素按照单位mol计算的比例应该为6.0:0.3:0.7。"
       ]
     }
