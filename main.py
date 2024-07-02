17725cd74ad98ccb5aa6c4c1d7a72bc8328d7d50
breadbread1984 <breadbread1984@163.com>
2024-07-02 10:11:37 +0800

    update

--- main.py
+++ main.py
@@ -3,28 +3,32 @@
 from absl import flags, app
 from reaction_path import PrecursorsRecommendation
 from instructions import Instructions
+from prompts import exp_instruction_prompt
+from models import Llama3, CodeLlama, Qwen2, CodeQwen1_5, Qwen1_5
 
 FLAGS = flags.FLAGS
 
 def add_options():
-  flags.DEFINE_string('host', default = 'neo4j+s://ce5c573a.databases.neo4j.io:7687', help = 'neo4j host')
-  flags.DEFINE_string('user', default = 'neo4j', help = 'user name')
-  flags.DEFINE_string('password', default = 'RdKzW-YnyyRd5h1Q6OoRQUDza5edKXrmqDzSTN7QkZg', help = 'password')
-  flags.DEFINE_string('db', default = 'neo4j', help = 'database')
+  flags.DEFINE_enum('model', default = 'llama3', enum_values = {'llama3', 'codellama', 'qwen2', 'codeqwen', 'qwen1.5'}, help = 'available models')
   flags.DEFINE_string('target', default = 'Li6PS5Cl', help = 'electrolyte represented in format of chemical formula')
+  flags.DEFINE_boolean('locally', default = False, help = 'whether to run model locally')
 
 def main(unused_argv):
   recommend = PrecursorsRecommendation()
   target_formula = [FLAGS.target]
   all_predicts = recommend.call(target_formula = target_formula, top_n = 10)
   precursor_sets = all_predicts[0]['precursors_predicts']
-  exp = Instructions(host = FLAGS.host, username = FLAGS.user, password = FLAGS.password, db = FLAGS.db)
+  tokenizer, llm = {
+    'llama3': Llama3,
+    'codellama': CodeLlama,
+    'qwen2': Qwen2,
+    'codeqwen': CodeQwen1_5,
+    'qwen1.5': Qwen1_5,
+  }[FLAGS.model](FLAGS.locally)
+  template, parser = exp_instruction_prompt(tokenizer)
+  chain = template | llm | parser
   for idx, precursors in enumerate(precursor_sets):
-    instructions = exp.steps(FLAGS.target, precursors)
-    if instructions is None: continue
-    print('第%d种实验方案:' % (idx + 1))
-    for sid, step in enumerate(instructions):
-      print('%d)%s' % (sid, step))
+    pass
 
 if __name__ == "__main__":
   add_options()
